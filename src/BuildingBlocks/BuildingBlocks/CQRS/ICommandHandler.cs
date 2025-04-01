3abdcccd5d530a925ec83be58faba17bef824c1c
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-01 21:24:56 +0330

    Add Command and Query handler

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    /// <summary>
+    /// specify that the ICommandHandler has the TRequest and TResponse and the TRequest should be command with the return type TResponse
+    /// </summary>
+    /// <typeparam name="TCommand"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface ICommandHandler<in TCommand, TResponse> :
+        IRequestHandler<TCommand,TResponse>
+        where TCommand:ICommand<TResponse>
+        where TResponse : notnull
+    {
+        
+    }
+
+    /// <summary>
+    /// ICommandHandler for Command witch has no specific response type
+    /// </summary>
+    /// <typeparam name="ICommand"></typeparam>
+    public interface ICommandHandler<in ICommand>:
+        IRequestHandler<ICommand,Unit>
+        where ICommand : ICommand<Unit>
+    {
+
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    /// <summary>
+    /// IQuesryHandler for handler read operation 
+    /// </summary>
+    /// <typeparam name="TQuesry"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface IQueryHandler<in TQuesry,TResponse>:
+        IRequestHandler<TQuesry,TResponse>
+        where TQuesry : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+    
+}
