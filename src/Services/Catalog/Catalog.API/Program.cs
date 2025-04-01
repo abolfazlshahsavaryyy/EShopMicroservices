d38fb71602bf4f088bb02c9c61f1b9e3bd64f2e6
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-01 21:26:53 +0330

    add Carter to the Program.cs file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
+
 //add application configuration pipline
+app.MapCarter();
 
 app.Run();
