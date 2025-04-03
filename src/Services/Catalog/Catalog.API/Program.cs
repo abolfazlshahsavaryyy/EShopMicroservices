c1eb423172875af19cc6486417b7bbe292e5092a
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-03 10:41:24 +0330

    add registration of the medatR to program.cs file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 //add services
 builder.Services.AddCarter();
+builder.Services.AddMediatR(confg =>
+{
+    confg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
