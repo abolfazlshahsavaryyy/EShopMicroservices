1ed9d02f9458ea4200b1f72281ea25ebaf7076d2
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-13 16:36:25 +0330

    add new Exception

---
+++ src/Services/Catalog/Catalog.API/Exceptions/NotFoundProductException.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class NotFoundProductException:Exception
+    {
+        private object Id { get; set; }
+        public NotFoundProductException(object id) : base($"Product not found with id : {id}")
+        {
+            Id = id;
+        }
+    }
+}
