ee6f11112d63945bfd59c8c3d7d18cd8c11a8676
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-13 23:17:13 +0330

    add getProductByCategory endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Products.GetProductByCategory
+﻿
+using Remotion.Linq.Clauses.ResultOperators;
+
+namespace Catalog.API.Products.GetProductByCategory
 {
-    public class GetProductByCategoryEndpoint
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCategoryEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/product/category/{category}",
+                async (string category, ISender sender) =>
+            {
+                var result =await sender.Send(new GetProductByCategoryQuery(category));
+                var response = new GetProductByCategoryResponse(result.products);
+                return Results.Ok(response);
+            })
+                .WithName("GetProductByCategory")
+                .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product using category")
+                .WithDescription("Get Product using category");
+        }
     }
 }
