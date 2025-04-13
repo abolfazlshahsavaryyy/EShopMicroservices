c561ce76c3556d339e4afd148825ff71469a834e
abolfazl <abolfazl@DESKTOP-O0Q1SIK>
2025-04-13 23:03:41 +0330

    add GetProductByCategoryHandler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryResult(IEnumerable<Product> products);
+    public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCategoryResult>;
+    internal class GetProductByCategoryHandler
+        (IDocumentSession session,ILogger<GetProductByCategoryHandler> logger) :
+        IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"getProductByCategory has been called with Category{query.Category}");
+
+            var products = await session.Query<Product>()
+                .Where(p=>p.Category.Contains(query.Category)).ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
