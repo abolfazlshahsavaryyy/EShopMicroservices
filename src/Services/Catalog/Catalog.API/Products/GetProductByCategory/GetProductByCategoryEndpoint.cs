83d4878ca445007c80b26c674b367be0b8321bdc
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-04-14 15:55:42 +0330

    debug route of endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -8,18 +8,18 @@ namespace Catalog.API.Products.GetProductByCategory
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/product/category/{category}",
+            app.MapGet("/products/category/{category}",
                 async (string category, ISender sender) =>
             {
                 var result =await sender.Send(new GetProductByCategoryQuery(category));
                 var response = new GetProductByCategoryResponse(result.products);
                 return Results.Ok(response);
             })
-                .WithName("GetProductByCategory")
-                .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
-                .ProducesProblem(StatusCodes.Status400BadRequest)
-                .WithSummary("Get Product using category")
-                .WithDescription("Get Product using category");
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product using category")
+            .WithDescription("Get Product using category");
         }
     }
 }
