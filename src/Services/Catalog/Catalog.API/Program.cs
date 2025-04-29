cd1523ce977f95385b07b8aea304c44063b45f4b
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-04-29 14:15:22 +0330

    add healthcheck for postgres sql

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,10 @@ using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handler;
 using Carter;
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
 using Marten;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,15 +26,22 @@ builder.Services.AddMarten(option =>
     option.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
 }).UseLightweightSessions();
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
-}
+//for seed database
+//if (builder.Environment.IsDevelopment())
+//{
+//    builder.Services.InitializeMartenWith<CatalogInitialData>();
+//}
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection"));
 var app = builder.Build();
 
 
 //add application configuration pipline
 app.MapCarter();
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.UseExceptionHandler(option => { });
 //app.UseExceptionHandler(exceptionHandlerApp =>
 //{
