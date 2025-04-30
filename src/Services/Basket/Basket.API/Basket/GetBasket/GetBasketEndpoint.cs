df55d28de977e87027f775cfd8f13e5425dd53c6
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-04-30 22:50:26 +0330

    add endpoint of get baskets

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿using Basket.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketEndpoint
+    //public record GetBasketequest(string username);
+    public record GetBasketResponse(ShopingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{username}", async (string username, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(username));
+                var response = result.Adapt<GetBasketResponse>();
+                return Results.Ok(response);
+            }).WithName("GetAllBasket")
+            .Produces<GetBasketQuery>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Get all baskets");
+        }
     }
 }
