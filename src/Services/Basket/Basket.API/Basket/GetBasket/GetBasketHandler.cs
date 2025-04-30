56749d3666dd2799829d8dfa111d371d2f1f7e1f
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-04-30 15:36:37 +0330

    add handler for get basket

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShopingCart Cart);
+    public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+        {
+            //TODO get the basket from the database 
+            //var basket =await _repository.GetBasket(request.Username);
+            throw new NotImplementedException();
+        }
     }
 }
