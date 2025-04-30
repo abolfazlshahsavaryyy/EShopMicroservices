02556ad77c2e045af082444513378eaf7c31a941
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-04-30 14:50:25 +0330

    add file structure for basket feature to the web api

---
+++ src/Services/Basket/Basket.API/Models/ShopingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShopingCart
+    {
+        public string Username { get; set; } = default;
+        public List<ShopingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+        public ShopingCart(string Username)
+        {
+            this.Username = Username;
+        }
+        //required for create non username shoping cart
+        public ShopingCart()
+        {
+            
+        }
+    }
+}
+
