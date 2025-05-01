3f4eebc9cbfa54194f953882cc2b1646b9f4fb5e
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-01 13:57:25 +0330

    edit storeBasketEndpoint

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketEndpoint
+    public record StoreBasketRequest(ShopingCart Cart);
+    public record StoreBasketResponse(string Usename);
+    public class StoreBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<StoreBasketResponse>();
+                return Results.Created($"/basket/{response.Usename}", response);
+
+            }).WithName("Store Cart")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Creare or update Carts");
+        }
     }
 }
