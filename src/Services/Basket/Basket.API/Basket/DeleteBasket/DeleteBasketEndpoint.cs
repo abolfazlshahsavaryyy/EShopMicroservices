1f9821b9c9a68e81a76de9452a64ec7377efe9cc
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-01 14:25:45 +0330

    add delete cart endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,30 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{username:string}",
+                async (string username, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(username));
+                var response = result.Adapt<DeleteBasketResponse>();
+                if (response.IsSuccess == true)
+                {
+                    return Results.Ok(response);
+                }
+                else
+                {
+                    return Results.NotFound(response);
+                }
+            }).WithName("Delete Cart")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Cart by givin username");
+        }
     }
 }
