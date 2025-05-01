d09303b002fa52e6047105d4a8ffcbbdcbf5bfc3
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-01 14:26:04 +0330

    add delete cart handler

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using System.Windows.Input;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string Username):ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(c => c.Username).NotNull().NotEmpty().WithMessage("Username is Required");
+        }
     }
+    public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            //TODO delete the cart from the db and cash
+            return new DeleteBasketResult(true);
+        }
+    }
+
 }
