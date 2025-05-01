a7e92d6d34bece842034751dde26bfa6932c4271
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-01 18:50:28 +0330

    add basket repository pattern for BasketRepository with Marten

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.API.Data
+{
+    public class BasketRepository
+        (IDocumentSession session)
+        : IBasketRepository
+    {
+        public async Task<ShopingCart> GetBasket(string username, CancellationToken token = default)
+        {
+            var basket = await session.LoadAsync<ShopingCart>(username, token);
+            return basket is null? throw new BasketNotFoundException(username):basket;
+        }
+        public async Task<ShopingCart> StoreBasket(ShopingCart cart, CancellationToken token = default)
+        {
+            session.Store(cart);
+            await session.SaveChangesAsync(token);
+            return cart;
+
+        }
+        public async Task<bool> DeleteBasket(string username, CancellationToken token = default)
+        {
+            session.Delete<ShopingCart>(username);
+            await session.SaveChangesAsync(token);
+            return true;
+
+
+        }
+
+        
+
+        
+    }
+}
---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        Task<ShopingCart> GetBasket(string username, CancellationToken token=default);
+        Task<ShopingCart> StoreBasket(ShopingCart cart, CancellationToken token = default);
+        Task<bool> DeleteBasket(string username, CancellationToken token = default);
+    }
+}
