3c571c229f269ba9890603e7e6528f1a9e818c73
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-01 14:26:22 +0330

    register carter and mediatR to the Program.cs file

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,17 +1,19 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
-app.Map("/", () =>
-{
-    var x=new
-    {
-        IsWork=true
-    };
-    return x;
-});
 app.Run();
