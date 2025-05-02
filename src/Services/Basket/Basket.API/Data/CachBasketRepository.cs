92f8ad76a04a59c8dcef6ec8144c091f444e25fe
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-02 20:59:13 +0330

    add decorator pattern in term of cache to the Basket service

---
+++ src/Services/Basket/Basket.API/Data/CachBasketRepository.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachBasketRepository 
+        (IBasketRepository repo,IDistributedCache cache)
+        : IBasketRepository
+    {
+        public async Task<ShopingCart> GetBasket(string username, CancellationToken token = default)
+        {
+            var cashedBasket = await cache.GetStringAsync(username, token);
+            if (!String.IsNullOrEmpty(cashedBasket))
+            {
+                return JsonSerializer.Deserialize<ShopingCart>(cashedBasket);
+            }
+            else
+            {
+                var basket=await repo.GetBasket(username, token);
+                await cache.SetStringAsync(username, JsonSerializer.Serialize(basket),token);
+                return basket;
+            }
+        }
+        public async Task<bool> DeleteBasket(string username, CancellationToken token = default)
+        {
+
+            var result= await repo.DeleteBasket(username, token);
+            await cache.RemoveAsync(username, token);
+            return result;
+        }
+
+        
+
+        public async Task<ShopingCart> StoreBasket(ShopingCart cart, CancellationToken token = default)
+        {
+            var basket=await repo.StoreBasket(cart, token);
+            await cache.SetStringAsync(cart.Username, JsonSerializer.Serialize(cart),token);
+            return basket;
+        }
+    }
+}
