80ded78664493fbf228dc5962a9cf84db2789e57
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-06 13:50:45 +0330

    add grpc service for discount

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            //TODO:get discount from the database
+            return base.GetDiscount(request, context);
+        }
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            //TODO Create the Discount to  Database
+            return base.CreateDiscount(request, context);
+        }
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            //TODO Update the given Discount
+            return base.UpdateDiscount(request, context);
+        }
+        public override Task<DeleteCouponResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            //TODO:Delete the Discount form the db
+            return base.DeleteDiscount(request, context);
+        }
+        
+    }
+}
