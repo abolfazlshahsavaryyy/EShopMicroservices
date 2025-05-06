ae72afe410b014a0c167ba7ef489b0da197ca59b
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-06 13:51:14 +0330

    add configuration for sqllite database using ef core

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext:DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> option):base(option)
+        {
+            
+        }
+        public DbSet<Coupon> coupons { get; set; }
+    }
+}
