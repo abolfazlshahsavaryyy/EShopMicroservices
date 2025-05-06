c5b9156e2a0b2a723a80b5d312f739df7211eace
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-07 01:38:02 +0330

    add seed data to dbContext

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -10,5 +10,13 @@ namespace Discount.Grpc.Data
             
         }
         public DbSet<Coupon> coupons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "its phone", Amount = 899},
+                new Coupon { Id = 2, ProductName = "IPhone 10", Description = "its good phone", Amount = 999 }
+                );
+        }
     }
 }
