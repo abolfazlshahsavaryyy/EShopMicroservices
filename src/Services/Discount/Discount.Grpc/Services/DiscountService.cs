9f3ab643d07c72d6a8353b1c12d3b059609ff63c
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-07 11:35:43 +0330

    implement the create,update,delete operation for dicount service

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -27,20 +27,70 @@ namespace Discount.Grpc.Services
             var couponModel = coupon.Adapt<CouponModel>();
             return couponModel;
         }
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
             //TODO Create the Discount to  Database
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "invalid request object"));
+            var result=await dbContext.coupons.AddAsync(coupon);
+            var rowAffect=await dbContext.SaveChangesAsync();
+            if (rowAffect > 0)
+            {
+                logger.LogInformation($"the new coupon has been added to db for product name : {request.Coupon.ProductName}");
+                return coupon.Adapt<CouponModel>();
+            }
+            else
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "can't add new coupon because of internal problem"));
+            }
+
+
         }
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
         {
             //TODO Update the given Discount
-            return base.UpdateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "invalid request object"));
+            var excisingCoupon= await dbContext.coupons.FindAsync(coupon.Id);
+            excisingCoupon.ProductName = coupon.ProductName;
+            excisingCoupon.Description = coupon.Description;
+            excisingCoupon.Amount = coupon.Amount;
+
+
+            var rowAffect = await dbContext.SaveChangesAsync();
+            if (rowAffect > 0)
+            {
+                logger.LogInformation($"the new coupon has been added to db for product name : {request.Coupon.ProductName}");
+                return coupon.Adapt<CouponModel>();
+            }
+            else
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "can't add new coupon because of internal problem"));
+            }
         }
-        public override Task<DeleteCouponResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        public override async Task<DeleteCouponResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
         {
             //TODO:Delete the Discount form the db
-            return base.DeleteDiscount(request, context);
+            var coupon = await dbContext.coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"Copoun with name : {request.ProductName} can't be find"));
+
+            dbContext.coupons.Remove(coupon);
+            var result=await dbContext.SaveChangesAsync();
+            if (result > 0)
+            {
+                logger.LogInformation($"coupon for product with name : {request.ProductName} has been deleted successfully");
+                return new DeleteCouponResponse { IsSuccess = true };
+            }
+            else
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "internal error happend while deleted the coupon"));
+                
+            }
+
         }
         
     }
