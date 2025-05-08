963459d9682002d5b875777f1356290f91795943
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-08 17:21:08 +0330

    add infrastructure layer of clean architecture

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection service,IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            return service;
+        }
+    }
+}
