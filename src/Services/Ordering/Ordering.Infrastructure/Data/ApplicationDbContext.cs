c53267c79dcccb81e1fbb227f2ced9a096a2ccd2
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-12 22:20:26 +0330

    add applicationDbContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+            
+
+        }
+        public DbSet<Customer> customers => Set<Customer>();
+        public DbSet<Order> orders => Set<Order>();
+        public DbSet<Product> products => Set<Product>();
+        public DbSet<OrderItem> orderItems => Set<OrderItem>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //this check all class witch has implement the IEntityType and apply them
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
