232527725ea1176e80a2473159e9750941c880bc
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-16 19:55:05 +0330

    fix: add specific id for each seed data

--- src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -22,7 +22,7 @@ namespace Catalog.API.Data
     {
         new Product
         {
-            Id = Guid.NewGuid(),
+            Id = new Guid("b94ed2b3-f94f-4458-bc49-8fc6d2e7f114"),
             Name = "Laptop",
             Category = new List<string> { "Electronics", "Computers" },
             Description = "High-performance laptop suitable for work and gaming.",
@@ -31,7 +31,7 @@ namespace Catalog.API.Data
         },
         new Product
         {
-            Id = Guid.NewGuid(),
+            Id =  new Guid("3c998547-cf15-4b5d-a635-291f3f7b5bc2"),
             Name = "Smartphone",
             Category = new List<string> { "Electronics", "Mobile Devices" },
             Description = "Latest model smartphone with cutting-edge features.",
@@ -40,7 +40,7 @@ namespace Catalog.API.Data
         },
         new Product
         {
-            Id = Guid.NewGuid(),
+            Id = new Guid("efb9e52f-3f12-4b57-bc44-7b7e69ff80e2"),
             Name = "Wireless Headphones",
             Category = new List<string> { "Electronics", "Audio" },
             Description = "Noise-cancelling over-ear headphones with long battery life.",
@@ -49,7 +49,7 @@ namespace Catalog.API.Data
         },
         new Product
         {
-            Id = Guid.NewGuid(),
+            Id = new Guid("6cf5bbf0-2558-4cf4-9f35-ec684b1f9aa7"),
             Name = "Smartwatch",
             Category = new List<string> { "Wearables", "Fitness" },
             Description = "Track your fitness goals with this stylish smartwatch.",
@@ -58,7 +58,7 @@ namespace Catalog.API.Data
         },
         new Product
         {
-            Id = Guid.NewGuid(),
+            Id = new Guid("8a83e15d-b4d2-4d99-bc98-2f409c5e2da8"),
             Name = "Gaming Mouse",
             Category = new List<string> { "Electronics", "Accessories" },
             Description = "Ergonomic mouse with customizable buttons and RGB lighting.",
