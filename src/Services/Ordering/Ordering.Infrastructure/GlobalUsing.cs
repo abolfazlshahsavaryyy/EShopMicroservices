4205cabc3ce79e2f361b25988b2069a5b0f2d455
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-20 15:43:23 +0330

    feat: add global using and delete extra using

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,5 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Ordering.Infrastructure.Data;
-
+global using FluentValidation;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects; 
