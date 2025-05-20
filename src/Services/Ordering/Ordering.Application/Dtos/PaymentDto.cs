284640fde11f694957a23d6a977dac5c34c0dd95
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-20 15:19:08 +0330

    feat: add Dto for models

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Dtos;
+
+
+public record PaymentDto
+    (
+    string CartName,
+    string CartNumber,
+    string Expiration,
+    string Cvv,
+    int PaymentMethod
+    );
