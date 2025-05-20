2dd8ca1dd54eea1b5fd97628f548a955a4a44453
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-20 15:19:33 +0330

    feat: add feature for create product

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using FluentValidation.Validators;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderResult(Guid Id); 
+public record CreateOrderCommand(OrderDto order)
+    :ICommand<CreateOrderResult>;
+
+public class CreateOrderCommandValidation : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidation()
+    {
+        RuleFor(x => x.order.OrderName).NotEmpty().WithMessage("order name is required");
+        RuleFor(x => x.order.CustomerId).NotNull().WithMessage("CustomerId is required");
+        RuleFor(x => x.order.OrderItems).NotEmpty().WithMessage("Order item can't be empty");
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        //create order entity from the given command that has order dto
+        //save to database
+        //return the result
+
+        throw new NotImplementedException();
+    }
+}
