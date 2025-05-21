b21a472d5ea9381a2269029628576245d7138b28
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-21 15:25:58 +0330

    feat: add basic handler for event of create and update (not completed)

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandler/OrderCreatedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandler;
+
+public class OrderCreatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Domain Event Handled:{notification.GetType().Name}");
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandler/OrderUpdatedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandler;
+
+public class OrderUpdatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderUpdateEvent>
+{
+    public Task Handle(OrderUpdateEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Domain Event Handled:{notification.GetType().Name}");
+        throw new NotImplementedException();
+    }
+}
