82a184efc49485f45f17c98dd9005f73ab37ab9e
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-21 11:58:09 +0330

    feat: add remove handler for order

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderResult(bool IsSuccess);
+public record DeleteOrderCommand
+    (Guid OrderId)
+    :ICommand<DeleteOrderResult>;
+
+public class DeleteOrderCommandValidation : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidation()
+    {
+        RuleFor(o => o.OrderId).NotEmpty().WithMessage("the order Id is required");
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler
+    (IApplicationDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = OrderId.Of(command.OrderId);
+        var order = await dbContext.Orders
+            .FindAsync([orderId],cancellationToken);
+        if(order is null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+        dbContext.Orders.Remove(order);
+        var result=await dbContext.SaveChangesAsync(cancellationToken);
+        if (result > 0)
+        {
+            return new DeleteOrderResult(true);
+        }
+        else
+        {
+            return new DeleteOrderResult(false);
+        }
+    }
+}
