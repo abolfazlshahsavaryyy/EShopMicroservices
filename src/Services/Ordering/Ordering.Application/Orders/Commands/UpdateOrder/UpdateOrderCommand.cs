b8f6752809ea4cba52ad38dc9bea73e242075399
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-21 11:48:31 +0330

    feat: add update command as feature update for order

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+public record UpdateOrderResult(bool IsSeccuss);
+public record UpdateOrderCommand(OrderDto OrderDto)
+    :ICommand<UpdateOrderResult>;
+
+public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(o => o.OrderDto.Id).NotEmpty().WithMessage("Id is required");
+        RuleFor(o => o.OrderDto.OrderName).NotEmpty().WithMessage("Order name is required");
+        RuleFor(o => o.OrderDto.CustomerId).NotEmpty().WithMessage("Customer id can't be empty");
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -0,0 +1,84 @@
+﻿
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderHandler
+    (IApplicationDbContext dbContext)
+    : ICommandHandler<UpdateOrderCommand, UpdateOrderResult>
+{
+    public async Task<UpdateOrderResult> Handle(UpdateOrderCommand command, CancellationToken cancellationToken)
+    {
+        //create Order object
+        //save changes
+        //return the result
+
+        var orderId = OrderId.Of(command.OrderDto.Id);
+        var order = await dbContext.Orders.
+            FindAsync([orderId], cancellationToken: cancellationToken);
+
+        if(order is null)
+        {
+            throw new OrderNotFoundException(command.OrderDto.Id);
+
+        }
+
+        UpdateOrderWithNewValue(order,command.OrderDto);
+
+        dbContext.Orders.Update(order);
+        var result=await dbContext.SaveChangesAsync(cancellationToken);
+        if (result > 0)
+        {
+            return new UpdateOrderResult(true);
+        }
+        else
+        {
+            return new UpdateOrderResult(false);
+        }
+
+
+    }
+
+    private void UpdateOrderWithNewValue(Order existingOrder, OrderDto newOrder)
+    {
+        var updateBillingAddress = Address.Of(
+            newOrder.BillingAddress.FirstName,
+            newOrder.BillingAddress.LastName,
+            newOrder.BillingAddress.EmailAddress,
+            newOrder.BillingAddress.AddressLine,
+            newOrder.BillingAddress.Country,
+            newOrder.BillingAddress.State,
+            newOrder.BillingAddress.ZipCode
+            );
+
+        var updpateShippingAddress = Address.Of
+            (
+            newOrder.ShippingAddress.FirstName,
+            newOrder.ShippingAddress.LastName,
+            newOrder.ShippingAddress.EmailAddress,
+            newOrder.ShippingAddress.AddressLine,
+            newOrder.ShippingAddress.Country,
+            newOrder.ShippingAddress.State,
+            newOrder.ShippingAddress.ZipCode
+            );
+
+        var updatePayment = Payment.Of
+            (
+            newOrder.Payment.CartName,
+            newOrder.Payment.CartNumber,
+            newOrder.Payment.Expiration,
+            newOrder.Payment.Cvv,
+            newOrder.Payment.PaymentMethod
+            );
+
+        existingOrder.Update
+            (
+            OrderName.Of(newOrder.OrderName),
+            updpateShippingAddress,
+            updateBillingAddress,
+            updatePayment,
+            status: newOrder.Status
+
+            );
+        
+    }
+}
