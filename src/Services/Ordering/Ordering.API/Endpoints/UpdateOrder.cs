315851174d4fac2bfc742faeb6a794a58bac777d
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-22 13:51:31 +0330

    configure the update order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.API.Endpoints
 {
-    public class UpdateOrder
+    public record UpdateOrderRequest(OrderDto order);
+    public record UpdareOrderReponse(bool IsSuccess);
+    public class UpdateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("orders", async (UpdateOrderRequest request,ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+                var result = await sender.Send(command);
+                var respones = result.Adapt<UpdareOrderReponse>();
+                return Results.Ok(respones);
+            })
+             .WithName("UpdateOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Order endpint")
+            .WithDescription("this enpoint enable to update existing order"); ;
+        }
     }
 }
