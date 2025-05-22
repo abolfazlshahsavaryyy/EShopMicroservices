0f69d85ada9273c6c8ef82dd8516b6b58fb3cef1
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-22 13:51:05 +0330

    feat: configure the create order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,6 +1,24 @@
-﻿namespace Ordering.API.Endpoints
+﻿namespace Ordering.API.Endpoints;
+
+public record CreateOrderRequest(OrderDto order);
+public record CreateOrderResponse(Guid orderId);
+public class CreateOrder : ICarterModule
 {
-    public class CreateOrder
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("orders/", async (CreateOrderRequest request,ISender sender) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+
+            var result = await sender.Send(command);
+
+            var respones = result.Adapt<CreateOrderResponse>();
+            return Results.Created($"orders/{respones.orderId}",respones);
+        })
+            .WithName("Create Order")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Order endpint")
+            .WithDescription("this ennpoint enable to create new order");
     }
 }
