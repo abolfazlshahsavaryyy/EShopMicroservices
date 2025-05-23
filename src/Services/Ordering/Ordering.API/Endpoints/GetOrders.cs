264bff520d6a47c4123a80b550d47e58f39e14a2
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-23 15:55:12 +0330

    feat: add get order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,22 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Paggination;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrderResponse(PaginatedResult<OrderDto> Orders);
+public class GetOrders : ICarterModule
 {
-    public class GetOrders
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("orders/", async ([AsParameters]PaginationRequest request,ISender sender) =>
+        {
+            var result = await sender.Send(request);
+            var response = request.Adapt<GetOrderResponse>();
+            return Results.Ok(response);
+        }).WithName("Get Order")
+        .Produces<GetOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders endpint")
+        .WithDescription("this enpoint enable to get orders");
     }
 }
