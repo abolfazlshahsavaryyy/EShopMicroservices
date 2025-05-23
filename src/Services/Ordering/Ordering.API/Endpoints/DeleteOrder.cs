d1a20fd42509d645c400b3e5655ac3adade42e52
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-23 15:55:01 +0330

    feat: add delete order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+public record DeleteOrderResponse(bool IsSuccess);
+public class DeleteOrder : ICarterModule
 {
-    public class DeleteOrder
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/orders/{id}", async(Guid id,ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(id));
+            var response = result.Adapt<DeleteOrderResponse>();
+            if (response.IsSuccess)
+            {
+                return Results.Ok(response);
+            }
+            else
+            {
+                return Results.BadRequest();
+            }
+        }).WithName("Delete Order")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Order endpint")
+            .WithDescription("this ennpoint enable to delete existing order");
     }
 }
